7b6b5c79e41764ab31df7365e475e4bf1fe6a52d
Loxodograph <paldino.nicholas@gmail.com>
2024-09-20 05:33:52 -0400

    touch screen try

--- index.js
+++ index.js
@@ -1,23 +1,25 @@
 const body = document.querySelector("body");
-// document.body.addEventListener("touchstart", function(e){ if (e.target.nodeName == 'CANVAS') { e.preventDefault(); } }, false);
-// document.body.addEventListener("touchend", function(e){ if (e.target.nodeName == 'CANVAS') { e.preventDefault(); } }, false);
-// document.body.addEventListener("touchmove", function(e){ if (e.target.nodeName == 'CANVAS') { e.preventDefault(); } }, false);
-// if (navigator.userAgent.match(/Android|BlackBerry|iPhone|iPad|iPod|Opera Mini|IEMobile/i)){
-//   body.addEventListener("touchmove", (event) => {
-//     const xPos = event.offsetX;
-//     const yPos = event.offsetY;
-//     const spanEl = document.createElement("span");
-//     spanEl.style.left = xPos + "px";
-//     spanEl.style.top = yPos + "px";
-//     const size = Math.random() * 100;
-//     spanEl.style.height = size + "px";
-//     spanEl.style.width = size + "px";
-//     body.appendChild(spanEl);
-//     setTimeout(() => {
-//       spanEl.remove();
-//     }, 3000);
-//   } )
-//   } else {
+document.body.addEventListener("touchstart", function(e){ if (e.target.nodeName == 'CANVAS') { e.preventDefault(); } }, false);
+document.body.addEventListener("touchend", function(e){ if (e.target.nodeName == 'CANVAS') { e.preventDefault(); } }, false);
+document.body.addEventListener("touchmove", function(e){ if (e.target.nodeName == 'CANVAS') { e.preventDefault(); } }, false);
+if (navigator.userAgent.match(/Android|BlackBerry|iPhone|iPad|iPod|Opera Mini|IEMobile/i)){
+  body.addEventListener("touchstart", (event) => {
+    console.log(event);
+    const xPos = event.pageX;
+    const yPos = event.pageY;
+    const spanEl = document.createElement("span");
+    spanEl.style.left = xPos + "px";
+    spanEl.style.top = yPos + "px";
+    const size = Math.random() * 100;
+    spanEl.style.height = size + "px";
+    spanEl.style.width = size + "px";
+    body.appendChild(spanEl);
+    setTimeout(() => {
+      spanEl.remove();
+    }, 3000);
+  }
+  )
+  } else {
 body.addEventListener("mousemove", (event) => {
     const xPos = event.offsetX;
     const yPos = event.offsetY;
@@ -32,4 +34,4 @@ body.addEventListener("mousemove", (event) => {
       spanEl.remove();
     }, 3000);
 } )
-  // }
+  }
