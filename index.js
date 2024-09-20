f276b8cd11f76ff4332659e6f602dc86819eea60
Loxodograph <paldino.nicholas@gmail.com>
2024-09-20 04:22:08 -0400

    added touchmove

--- index.js
+++ index.js
@@ -13,3 +13,18 @@ body.addEventListener("mousemove", (event) => {
       spanEl.remove();
     }, 3000);
 } )
+
+body.addEventListener("touchmove", (event) => {
+  const xPos = event.offsetX;
+  const yPos = event.offsetY;
+  const spanEl = document.createElement("span");
+  spanEl.style.left = xPos + "px";
+  spanEl.style.top = yPos + "px";
+  const size = Math.random() * 100;
+  spanEl.style.height = size + "px";
+  spanEl.style.width = size + "px";
+  body.appendChild(spanEl);
+  setTimeout(() => {
+    spanEl.remove();
+  }, 3000);
+} )
